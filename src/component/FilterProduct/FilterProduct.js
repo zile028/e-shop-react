ea3c63d8f856e74d8b8837cb0c7a365732eca8ca
Zile028 <zile028@gmail.com>
2022-09-01 09:47:51 +0200

    filter product

--- src/component/FilterProduct/FilterProduct.js
+++ src/component/FilterProduct/FilterProduct.js
@@ -1,23 +1,46 @@
-import React from "react";
+import {useState} from "react";
 import "./filterProduct.scss";
+import {useDispatch} from "react-redux";
+import {setFiltered} from "../../redux/productsSlice";
+
 function FilterProduct() {
-  return (
-    <div className="filter-wrapper">
-      <button>Filter</button>
-      <div className="filter-options">
-        <select className="filter-option-category" name="" id="">
-          <option defaultChecked disabled>
-            Category
-          </option>
-          <option value="">Phone</option>
-          <option value="">Laptop</option>
-        </select>
-        <div className="filter-option-price">
-          <input type="range" min={0} max={100} />
-        </div>
-      </div>
-    </div>
-  );
+	const [filter, setFilter] = useState({});
+	const dispatch = useDispatch
+
+	function applyFilter() {
+		dispatch(setFiltered(filter))
+	}
+
+	function onInputHandler(e) {
+		let copyFilter = {...filter}
+		copyFilter[e.target.name] = e.target.value
+		setFilter(copyFilter)
+	}
+
+	return (
+	  <div className="filter-wrapper">
+		  <button>Filter</button>
+		  <div className="filter-options">
+			  <select
+				name="category"
+				className="filter-options-category"
+				defaultValue={"default"}
+				onChange={onInputHandler}
+			  >
+				  <option value={"default"} disabled>
+					  Category
+				  </option>
+				  <option value="Phone">Phone</option>
+				  <option value="Laptop">Laptop</option>
+			  </select>
+			  <div className="filter-options-price">
+				  <span>Price less then:</span><span>0$</span>
+				  <input type="range" min={0} max={300000} name="priceLimit" onChange={onInputHandler}/>
+			  </div>
+			  <button onClick={applyFilter}>Apply</button>
+		  </div>
+	  </div>
+	);
 }
 
 export default FilterProduct;
--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -1,16 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import {createSlice} from "@reduxjs/toolkit";
 
 const productsSlice = createSlice({
-  name: "products",
-  initialState: {
-    products: [],
-  },
-  reducers: {
-    setProducts(state, action) {
-      state.products = action.payload;
-    },
-  },
+	name: "products",
+	initialState: {
+		products: [],
+		filtered: []
+	},
+	reducers: {
+		setProducts: (state, action) => {
+			state.products = action.payload;
+		},
+		setFiltered: (state, action) => {
+			console.log(action.payload)
+		}
+	},
 });
 
-export const { setProducts } = productsSlice.actions;
+export const {setProducts, setFiltered} = productsSlice.actions;
 export default productsSlice.reducer;
