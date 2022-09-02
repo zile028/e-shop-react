b57946a960c86657994cc7f7afc723f62ee52508
zile028 <zile028@gmail.com>
2022-09-02 08:10:03 +0200

    filter product

--- src/component/FilterProduct/FilterProduct.js
+++ src/component/FilterProduct/FilterProduct.js
@@ -1,54 +1,90 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import "./filterProduct.scss";
-import {useDispatch, useSelector} from "react-redux";
-import {setFiltered} from "../../redux/productsSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { setCategory, setFiltered } from "../../redux/productsSlice";
+import { useSearchParams } from "react-router-dom";
+import { useDelayUnmount } from "../../hooks/useDelayUnmount";
 
 function FilterProduct() {
-	const [filter, setFilter] = useState({
-		category: null,
-		priceLimit: 0
-	});
-	const dispatch = useDispatch()
-	const {filtered} = useSelector(state => state.productsStore)
+  const { category } = useSelector((state) => state.productsStore);
+  const [searchParam, setSearchParam] = useSearchParams();
+  const [filter, setFilter] = useState({
+    category: null,
+    priceLimit: 0,
+  });
+  const dispatch = useDispatch();
+  const [isMounted, setIsMounted] = useState(false);
+  const renderFilter = useDelayUnmount(isMounted, 300);
+  const mountedStyle = {
+    animationName: "inFilter",
+    animationDuration: "300ms",
+    transformOrigin: "left top",
+  };
 
-	useEffect(() => {
-		console.log(filtered)
-	}, [filtered])
+  const unmountedStyle = {
+    animationName: "outFilter",
+    animationDuration: "320ms",
+    transformOrigin: "left top",
+  };
 
-	function applyFilter() {
-		dispatch(setFiltered(filter))
-	}
+  useEffect(() => {
+    dispatch(setCategory());
+  }, []);
 
-	function onInputHandler(e) {
-		let copyFilter = {...filter}
-		copyFilter[e.target.name] = e.target.value
-		setFilter(copyFilter)
-	}
+  function applyFilter() {
+    dispatch(setFiltered(filter));
+    setSearchParam({ ...searchParam, page: 1 });
+    setIsMounted(false);
+  }
 
-	return (
-	  <div className="filter-wrapper">
-		  <button>Filter</button>
-		  <div className="filter-options">
-			  <select
-				name="category"
-				className="filter-options-category"
-				defaultValue={"default"}
-				onChange={onInputHandler}
-			  >
-				  <option value={"default"} disabled>
-					  Category
-				  </option>
-				  <option value="smartphones">smartphones</option>
-				  <option value="laptops">laptops</option>
-			  </select>
-			  <div className="filter-options-price">
-				  <span>Price less then:</span><span>{filter.priceLimit}$</span>
-				  <input type="range" min={0} max={3000} name="priceLimit" onChange={onInputHandler}/>
-			  </div>
-			  <button onClick={applyFilter}>Apply</button>
-		  </div>
-	  </div>
-	);
+  function onInputHandler(e) {
+    let copyFilter = { ...filter };
+    copyFilter[e.target.name] = e.target.value;
+    setFilter(copyFilter);
+  }
+
+  return (
+    <div className="filter-wrapper">
+      <button onClick={() => setIsMounted(!isMounted)}>Filter</button>
+      {renderFilter && (
+        <div
+          style={isMounted ? mountedStyle : unmountedStyle}
+          className="filter-options"
+        >
+          <select
+            name="category"
+            className="filter-options-category"
+            defaultValue={"default"}
+            onChange={onInputHandler}
+          >
+            <option value={"default"} disabled>
+              Category
+            </option>
+            {category.map((el, index) => {
+              return (
+                <option key={index} value={el}>
+                  {el}
+                </option>
+              );
+            })}
+            {/* <option value="laptops">laptops</option> */}
+          </select>
+          <div className="filter-options-price">
+            <span>Price less then:</span>
+            <span>{filter.priceLimit}$</span>
+            <input
+              type="range"
+              min={0}
+              max={3000}
+              name="priceLimit"
+              onChange={onInputHandler}
+            />
+          </div>
+          <button onClick={applyFilter}>Apply</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default FilterProduct;
--- src/redux/productsSlice.js
+++ src/redux/productsSlice.js
@@ -1,21 +1,34 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const productsSlice = createSlice({
-	name: "products",
-	initialState: {
-		products: [],
-		filtered: []
-	},
-	reducers: {
-		setProducts: (state, action) => {
-			state.products = action.payload;
-			state.filtered = [...action.payload]
-		},
-		setFiltered: (state, {payload}) => {
-			state.filtered = state.products.filter(el => (payload.category === null ? true : el.category === payload.category) && el.price < payload.priceLimit)
-		}
-	},
+  name: "products",
+  initialState: {
+    products: [],
+    filtered: [],
+    category: [],
+  },
+  reducers: {
+    setProducts: (state, { payload }) => {
+      state.products = payload;
+      state.filtered = [...payload];
+    },
+    setCategory: (state, action) => {
+      let cat = new Set();
+      state.products.forEach((el) => {
+        cat.add(el.category);
+      });
+      state.category = [...cat];
+    },
+    setFiltered: (state, { payload }) => {
+      state.filtered = state.products.filter((el) =>
+        (payload.category === null ? true : el.category === payload.category) &&
+        payload.priceLimit === 0
+          ? true
+          : el.price < payload.priceLimit
+      );
+    },
+  },
 });
 
-export const {setProducts, setFiltered} = productsSlice.actions;
+export const { setProducts, setFiltered, setCategory } = productsSlice.actions;
 export default productsSlice.reducer;
